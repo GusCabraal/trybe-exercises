5834e9298eecbeb974c2a935012b63e5b0b4e247
Cabral <cabraloko@pop-os.localdomain>
2022-03-07 18:50:38 -0300

    primeiros 2 exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/pratica-bloco-04-introducao-a-javascript-e-logica-de-programacao/1.1-adicao.js
@@ -0,0 +1,6 @@
+const a = 10;
+const b = 5;
+let soma = a + b;
+
+
+console.log("O resultado de " + a + " + " + b + " vale: "+ soma)
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/pratica-bloco-04-introducao-a-javascript-e-logica-de-programacao/1.2-subtracao.js
@@ -0,0 +1,5 @@
+const a = 8;
+const b = 2;
+let subtracao = a - b;
+
+console.log("O resultado de " + a + " - " + b + " vale: "+ subtracao)

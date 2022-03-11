c57ce397bf1709470d5fb7bd5eb1271aa598b877
Cabral <cabraloko@pop-os.localdomain>
2022-03-10 22:18:21 -0300

    parte I concluida - objetos e for/in

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-dia-4-javascript-objetos-e-funcoes/parte-2-1-funcoes.js
@@ -0,0 +1,11 @@
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+let word = 'trybe'
+
+function verificaPalindrome (palavra){
+    for (let i =0; i <= palavra.length; i+= 1){
+        console.log(i)
+    }
+}
+
+verificaPalindrome(word)

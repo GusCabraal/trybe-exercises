19565ca7f00e069d00c10145a08ae2eb6e812ada
GusCabraal <gustavocabral97@hotmail.com>
2022-04-01 19:09:29 -0300

    exercicio 2-2 concluido

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2-2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2-2.js
@@ -1,5 +1,10 @@
 const longestWord = phrase => {
-    
+    const words = phrase.split(' ');
+    let maiorPalavra = words[0];
+    for (let i = 0; i < words.length; i += 1 ){
+    words[i].length > maiorPalavra.length ? maiorPalavra = words[i]: false;
+    } 
+    return maiorPalavra;
 }
 
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu')); // retorna 'aconteceu'

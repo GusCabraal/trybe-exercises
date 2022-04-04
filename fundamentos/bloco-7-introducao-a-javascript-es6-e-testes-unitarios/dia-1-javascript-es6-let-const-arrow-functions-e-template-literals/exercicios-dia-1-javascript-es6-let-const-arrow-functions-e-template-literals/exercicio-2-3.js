871ddb16924878f4b47e33698e378a6d0dbc39f9
GusCabraal <gustavocabral97@hotmail.com>
2022-04-04 13:28:09 -0300

    exercicio 2.3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2-3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio-2-3.js
@@ -1,7 +1,6 @@
 const butao = document.getElementById('botao-contagem');
 const texto = document.getElementById('text');
 let clickCount = 0;
-butao.addEventListener('click', contagem = () => {
-    clickCount += 1;
-    texto.innerText = `Contagem : ${clickCount}`;
+butao.addEventListener('click', () => {
+    texto.innerText = `Contagem : ${clickCount += 1}`;
 })

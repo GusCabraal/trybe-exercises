5fcd1be0d61cb2a55bbc1286061a5a5841b196e0
GusCabraal <gustavocabral97@hotmail.com>
2022-04-14 10:44:53 -0300

    exemplos da aula concluidos

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/aula-dia-4-javascript-es6-higher-order-functions-reduce/exemplo5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/aula-dia-4-javascript-es6-higher-order-functions-reduce/exemplo5.js
@@ -1,11 +1,6 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
-const evenNumbers = (sum, number) => {
-  if (number % 2 === 0) {
-    sum += number;
-  }
-  return sum;
-};
+const evenNumbers = (sum, number) => (number % 2 === 0) ? sum += number : sum ;
 
 const sumEvenNumbers = numbers.reduce(evenNumbers);
 

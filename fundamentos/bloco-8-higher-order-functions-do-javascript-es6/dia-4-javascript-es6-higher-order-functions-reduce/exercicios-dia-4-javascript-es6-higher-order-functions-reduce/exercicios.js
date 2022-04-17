65af2643e008cd40379755cf06c2591f7ae1a40e
GusCabraal <gustavocabral97@hotmail.com>
2022-04-17 15:27:18 -0300

    exercicio 8.5 - 8

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios-dia-4-javascript-es6-higher-order-functions-reduce/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicios-dia-4-javascript-es6-higher-order-functions-reduce/exercicios.js
@@ -85,24 +85,24 @@ function reduceNames() {
   return books.reduce((acc, curr) => {
    if(curr === books[books.length -1 ]) return `${acc}, ${curr.author.name}.`
    return `${acc}, ${curr.author.name}`;
-})
+})}
 
 console.log(reduceNames());
 
-// // 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 
-// const names = [
-//     'Aanemarie', 'Adervandes', 'Akifusa',
-//     'Abegildo', 'Adicellia', 'Aladonata',
-//     'Abeladerco', 'Adieidy', 'Alarucha',
-//   ];
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
   
-//   function containsA() {
-//   // escreva seu código aqui
-//     return names.map(( name) => 
-//          name.toLowerCase().split('').reduce((result, letra) => {
-//             if(letra === 'a') return result + 1;
-//             return result;
-//         }, 0),0);
-// }
-// console.log(containsA());
+  function containsA() {
+  // escreva seu código aqui
+    return names.map(( name) => 
+         name.toLowerCase().split('').reduce((result, letra) => {
+            if(letra === 'a') return result + 1;
+            return result;
+        }, 0),0);
+}
+console.log(containsA());
--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/aula-dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/pra-fixar4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/aula-dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/pra-fixar4.js
@@ -8,3 +8,11 @@ console.log(comida, animal, bebida); // gato água arroz
 
 [comida, animal, bebida] = [bebida, comida, animal]; // Criando o array e o desestruturando
 console.log(comida, animal, bebida); // arroz gato água
+
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // 0
+console.log(position2d); // 0

60c4394d053d340eb00daa50823d856e1d4cfec8
GusCabraal <gustavocabral97@hotmail.com>
2022-05-08 19:56:38 -0300

    primeira versão da pokedex

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemons.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Component } from "react";
+
+class Pokedex extends Component {
+  render() {
+    const { name, type, image, averageWeight } = this.props.pokemon;
+    const { value: peso, measurementUnit: un } = averageWeight;
+    return (
+      <div className="card">
+        <div>
+          <p>{name}</p>
+          <p>{type}</p>
+          <p>{`${peso} ${un}`}</p>
+        </div>
+          <img src={image} alt={`${name} sprite`} />
+      </div>
+    );
+  }
+}
+
+export default Pokedex;

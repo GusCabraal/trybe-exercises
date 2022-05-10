3d42496f5430352c059c1bf6a68ea7945d520724
GusCabraal <gustavocabral97@hotmail.com>
2022-05-10 01:26:20 -0300

    exemplos de fixação concluidos

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/aula-11.1/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/aula-11.1/src/App.js
@@ -4,29 +4,39 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+
     this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
+
+    }
     this.handleButtonTwo = this.handleButtonTwo.bind(this);
     this.handleButtonThree = this.handleButtonThree.bind(this);
   }
 
   handleButtonOne = () => {
-    console.log(this)
-    console.log("Clicou no botão 1!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }))
   }
   handleButtonTwo = () => {
-    console.log(this)
-    console.log("Clicou no botão 2!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }))
   }
   handleButtonThree = () => {
-    console.log(this)
-    console.log("Clicou no botão 3!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }))
   }
     render() {
     return (
       <div>
-            <button onClick={ this.handleButtonOne }>Botão 1</button>
-            <button onClick={ this.handleButtonTwo }>Botão 2</button>
-            <button onClick={ this.handleButtonThree }>Botão 3</button>
+            <button onClick={ this.handleButtonOne }>{this.state.numeroDeCliques1}</button>
+            <button onClick={ this.handleButtonTwo }>{this.state.numeroDeCliques2}</button>
+            <button onClick={ this.handleButtonThree }>{this.state.numeroDeCliques3}</button>
       </div>
     );
   }

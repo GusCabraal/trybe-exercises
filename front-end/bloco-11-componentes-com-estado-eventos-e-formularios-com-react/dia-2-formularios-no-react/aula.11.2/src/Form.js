0a9c4cd5f145f6b2a66eb362a586e40922f11d3d
GusCabraal <gustavocabral97@hotmail.com>
2022-05-12 17:38:11 -0300

    exemplo 4 concluido

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/aula.11.2/src/Form.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/aula.11.2/src/Form.js
@@ -1,41 +1,97 @@
-import React, { Component } from 'react'
-import './Form.css'
+import React, { Component } from "react";
+import "./Form.css";
 
 class Form extends Component {
   constructor() {
     super();
 
-    this.handleChange = this.handleChange.bind(this);
-
     this.state = {
-      estadoFavorito: '',
+      nome: "",
+      email: "",
+      age: 0,
+      anecdote: "",
+      agree: false,
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
 
-
-  handleChange(event) {
     this.setState({
-      estadoFavorito: event.target.value,
+      [name]: value,
     });
   }
 
   render() {
+    const { nome, email, age, anecdote, agree} = this.state;
+
     return (
       <div>
-        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <h1>
+          Estados e React - Tecnologia fantástica ou reagindo a regionalismos?
+        </h1>
         <form className="form">
-          <label>
-            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+          <label htmlFor="name">
+            Nome:
+            <input
+              id="name"
+              type="text"
+              name="nome"
+              onChange={this.handleChange}
+              value={nome}
+            />
           </label>
-          <input
-            type="number"
-            name="idade"
-          />
-          <input
-            type="checkbox"
-            name="vaiComparecer"
-          />
+
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={this.handleChange}
+              value={email}
+            />
+          </label>
+
+          <label htmlFor="age">
+            Idade:
+            <select
+              id="age"
+              name="age"
+              defaultValue=""
+              onChange={this.handleChange}
+              value={age}
+            >
+              <option value="">Selecione</option>
+              <option value="adult">Maior que 18</option>
+              <option value="underage">Menor que 18</option>
+            </select>
+          </label>
+
+          <label htmlFor="anecdote">
+            Anedota:
+            <textarea
+              id="anecdote"
+              name="anecdote"
+              onChange={this.handleChange}
+              value={anecdote}
+            />
+          </label>
+          <label>
+            Adicione os arquivos extras
+            <input type="file" />
+          </label>
+          <label>
+            Concordo com os termos
+            <input
+              type="checkbox"
+              name="agree"
+              onChange={this.handleChange}
+              value={agree}
+            ></input>
+          </label>
         </form>
       </div>
     );

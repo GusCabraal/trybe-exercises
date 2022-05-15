cce5eac472a3a802b3162df4e1ecdf2097c7c4d6
GusCabraal <gustavocabral97@hotmail.com>
2022-05-14 21:32:32 -0300

    pokedex concluida

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex/src/components/Pokedex.js
@@ -1,14 +1,77 @@
-import { Component } from 'react';
-import Pokemon from './Pokemon';
+import { Component } from "react";
+import Pokemon from "./Pokemon";
 
 class Pokedex extends Component {
-    render() {
-        const {pokemons} = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+        //todos os pokemons
+      pokemons: props.pokemons,
+      
+      currentIndex: 0,
+      //pokemons filtrados
+      myListPokemons: props.pokemons,
+      filter: '',
+    };
+  }
+  next() {
+    this.setState((prevState) => {
+        let nextIndex = prevState.currentIndex + 1
+        if (prevState.myListPokemons.length - 1 === prevState.currentIndex)
+        nextIndex = 0
+        return {
+            currentIndex: nextIndex
+        } 
+    })
+  }
+  filterPokemon(Filter) {
+      this.setState({ 
+          filter: Filter,
+          myListPokemons: this.state.pokemons.filter(({ type }) => {
+              if(Filter === '')
+              return true
+                return type === Filter
+      }),
+      currentIndex: 0,
+    })
+  }
+  listHasMoreThanOne (){
+    return this.state.myListPokemons.length === 1;
+  }
+
+  getTypesPokemons(){
+    const types = this.state.pokemons.map(({ type }) => type);
+    return([...new Set(types)].sort());
+  }
+
+  render() {
+    const { currentIndex, myListPokemons } = this.state;
     return (
-        <div className='cardlist'>
-            {pokemons.map(pokemon => <Pokemon key ={pokemon.id} pokemon = {pokemon}/>)}
-        </div>)
-    }
+      <>
+        <div>
+            <button onClick={() => this.filterPokemon('')}>All</button>
+
+            {this.getTypesPokemons().map((pokemon) => 
+          <button key={ pokemon } onClick={() => this.filterPokemon(pokemon)}>{pokemon}</button>)}
+
+        </div>
+        <div className="cardlist">
+          {
+            <Pokemon
+              key={ myListPokemons[currentIndex].id}
+              pokemon={myListPokemons[currentIndex]}
+            />
+          }
+        </div>
+        <div>
+          <button
+            onClick={() => this.next()}
+            disabled={ this.listHasMoreThanOne() }
+            >Próximo</button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Pokedex;

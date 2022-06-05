d9242e67aab0caffa80ddf10a257514ab813a900
GusCabraal <gustavocabral97@hotmail.com>
2022-06-05 01:22:52 -0300

    parte 1 concluida

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service2.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service2.js
@@ -1,7 +1,6 @@
 const service = require("./service.js");
 jest.mock("./service.js");
 
-
 describe("Testa uma função que gera um número aleatório", () => {
   test("Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.", () => {
     service.randomNumber = jest.fn().mockReturnValue(10);

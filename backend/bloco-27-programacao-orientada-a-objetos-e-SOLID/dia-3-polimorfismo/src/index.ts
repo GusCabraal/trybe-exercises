b60a95c835691ae82b66175cba8eab373ca593b7
GusCabraal <gustavocabral97@hotmail.com>
2022-10-13 19:29:36 -0300

    criado arquivos person e student

---
+++ backend/bloco-27-programacao-orientada-a-objetos-e-SOLID/dia-3-polimorfismo/src/index.ts
@@ -0,0 +1,7 @@
+// ./index.ts
+
+import Person from './Person';
+import Student from './Student';
+
+const carolina = new Student('Carolina da Silva', new Date('2005/03/17'));
+console.log(carolina);
